c98ff59c1281264c362ba105d4769f716f7505e1
lewis520lys <lewis520lys@163.com>
2017-04-19 11:13:39 +0800

    ssss

---
+++ app/src/main/java/com/lewis/daiosi/diaosicheng/fragment/SettingFragment.java
@@ -0,0 +1,32 @@
+package com.lewis.daiosi.diaosicheng.fragment;
+
+import android.content.Context;
+import android.view.View;
+
+import com.lewis.daiosi.diaosicheng.base.BaseFragment;
+
+/**
+ * Created by Administrator on 2017/4/19 0019.
+ */
+
+public class SettingFragment extends BaseFragment {
+    @Override
+    public int bindLayout() {
+        return 0;
+    }
+
+    @Override
+    public void initView(View view) {
+
+    }
+
+    @Override
+    public void doBusiness(Context mContext) {
+
+    }
+
+    @Override
+    public void widgetClick(View v) {
+
+    }
+}
